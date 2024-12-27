134241d9e75b45d12488437574d51d0d8295d807
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2024-12-27 20:23:44 +0530

    Improved app_test.py
    Fixed bugs that prevented dash a app from fetching data and rendering graph. Now, the data just needs to be displayed correctly for the plotly graph, tickers need to be defined for the sql db and the dropdown menu to contain multiple ticker options.

--- app_test.py
+++ app_test.py
@@ -1,13 +1,14 @@
 import sqlite3
 import plotly.graph_objs as go
 import pandas as pd  # don't forget to import pandas too!
-from dash import dcc
-from dash import html
+from dash import dcc, html, Dash
+from dash.dependencies import Input, Output
 
 # Connect to your SQLite database
 conn = sqlite3.connect('market_data.db')
 
 # Define a function to fetch data from the database
+app = Dash(__name__)
 
 
 def get_data(ticker):
@@ -15,16 +16,19 @@ def get_data(ticker):
     cursor = conn.cursor()
     cursor.execute(query, (ticker,))
     rows = cursor.fetchall()
-    return pd.DataFrame(rows)
+    data = pd.DataFrame(rows)
+    data.columns = ["ticker", "datetime", "open", "high", "low", "close", "volume"]
+    return data
 
 
+print(f"data fetched: {get_data('NQ_4H.csv')}")
 # Define the layout of your app
 app.layout = html.Div([
     dcc.Dropdown(
         id='ticker-dropdown',
         options=[{'label': ticker, 'value': ticker}
-                 for ticker in get_data('all')['ticker'].unique()],
-        value=get_data('all')['ticker'].unique()[0]
+                 for ticker in get_data('NQ_4H.csv')['ticker'].unique()],
+        value=get_data('NQ_4H.csv')['ticker'].unique()[0]
     ),
     dcc.Graph(id='market-data-graph')
 ])
@@ -40,3 +44,8 @@ def update_graph(ticker):
     data = get_data(ticker).set_index('datetime')[['close']]
     fig = go.Figure(data=[go.Scatter(x=data.index, y=data['close'])])
     return fig
+
+
+# run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
